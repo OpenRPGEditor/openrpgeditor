b45d40ec21746c45fc65d24b5a09534baeb00152
Phillip Stephens <antidote.crk@gmail.com>
2024-06-17 22:50:40 -0700

    Fix click-throughs into the event list

--- src/core/Core/EventListView.cpp
+++ src/core/Core/EventListView.cpp
@@ -19,12 +19,16 @@ void EventListView::draw() {
             m_parent->mapEditor()->setSelectedEvent(&*event);
             selectedHere = true;
           }
+
           if (m_parent->mapEditor()->selectedEvent() == &event.value() && !selectedHere &&
               !m_parent->mapEditor()->scrolledToEvent()) {
             ImGui::SetScrollHereY();
           }
-          if (ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left) && ImGui::IsItemActivated()) {
-            m_parent->mapEditor()->addEventEditor(EventEditor(m_parent, &event.value()));
+
+          if (ImGui::IsWindowFocused()) {
+            if (ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left) && ImGui::IsItemActivated()) {
+              m_parent->mapEditor()->addEventEditor(EventEditor(m_parent, &event.value()));
+            }
           }
         }
       }

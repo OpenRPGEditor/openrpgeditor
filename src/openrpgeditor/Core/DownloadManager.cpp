d188e58636f47f2e3bbcfd1b5dd6f1f130423d89
Phillip Stephens <antidote.crk@gmail.com>
2024-11-29 14:13:30 -0800

    Start DownloadManager imp

--- src/openrpgeditor/Core/DownloadManager.cpp
+++ src/openrpgeditor/Core/DownloadManager.cpp
@@ -1,1 +1,27 @@
 #include "Core/DownloadManager.hpp"
+
+void writeCallback(char* ptr, size_t size, size_t nmemb, void* userdata) {
+  const auto entry = static_cast<DownloadManager::DownloadEntry*>(userdata);
+  fwrite(ptr, size, nmemb, entry->fileHandle());
+  entry->addBytesDownloaded(size * nmemb);
+}
+
+DownloadManager::DownloadEntry::DownloadEntry(const std::string_view url, const std::string_view destination) : m_url(url), m_destinationPath(destination) {
+  m_destination = fopen(m_destinationPath.data(), "wb");
+  m_destinationPath = destination;
+  m_curlHandle = curl_easy_init();
+
+  if (m_curlHandle) {}
+}
+
+DownloadManager::DownloadManager() {
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+  m_multiHandle = curl_multi_init();
+}
+
+DownloadManager::~DownloadManager() {
+  curl_multi_cleanup(m_multiHandle);
+  curl_global_cleanup();
+}
+
+void DownloadManager::addDownload(const std::string_view url, const std::string_view destination) { m_curlHandles.emplace_back(url, destination); }
--- src/openrpgeditor/Core/DownloadManager.hpp
+++ src/openrpgeditor/Core/DownloadManager.hpp
@@ -1,20 +1,41 @@
 #pragma once
 
 #include <curl/curl.h>
+#include <fstream>
 #include <string>
 #include <vector>
+
 class DownloadManager {
 public:
-  struct DownloadEntry {
-    std::string url;
-    CURL* curlHandle;
-    curl_off_t bytesDownloaded;
-    curl_off_t bytesTotal;
+  class DownloadEntry {
+  public:
+    DownloadEntry(std::string_view url, std::string_view destination);
+    ~DownloadEntry();
+
+    CURL* handle() const { return m_curlHandle; }
+    std::string_view url() const { return m_url; }
+    FILE* fileHandle() const { return m_destination; }
+    curl_off_t bytesDownloaded() const { return m_bytesDownloaded; }
+    curl_off_t bytesTotal() const { return m_bytesTotal; }
+
+    void addBytesDownloaded(const curl_off_t bytesDownloaded) { m_bytesDownloaded += bytesDownloaded; }
+
+  private:
+    std::string m_url;
+    std::string m_destinationPath;
+    FILE* m_destination = nullptr;
+    CURL* m_curlHandle = nullptr;
+    curl_off_t m_bytesDownloaded{};
+    curl_off_t m_bytesTotal{};
+    int m_curlResult{CURLE_OK};
   };
 
   DownloadManager();
+  ~DownloadManager();
+
+  void addDownload(std::string_view url, std::string_view destination);
 
 private:
   CURLM* m_multiHandle;
-  std::vector<CURL*> m_curlHandles;
+  std::vector<DownloadEntry> m_curlHandles;
 };

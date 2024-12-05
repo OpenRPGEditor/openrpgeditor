88f3cc21137f71a05d79bff8c73abfdac9a38d2f
Phillip Stephens <antidote.crk@gmail.com>
2024-12-05 14:35:45 -0800

    Add missing ITileView files

---
+++ src/openrpgeditor/Core/Tilemap/ITileView.cpp
@@ -0,0 +1,15 @@
+﻿#include "Core/Tilemap/ITileView.hpp"
+
+#include "Core/Application.hpp"
+#include "Core/Graphics/Texture.hpp"
+
+#include <SDL.h>
+
+RenderImage ITileView::createRenderTexture(const int width, const int height) { return {width, height}; }
+
+void ITileView::clearRect(RenderImage& painter, const RectF& rect) {
+  const auto oldComposition = painter.compositionMode();
+  painter.setCompositionMode(RenderImage::CompositionMode::Clear);
+  painter.eraseRect(rect);
+  painter.setCompositionMode(oldComposition);
+}
---
+++ src/openrpgeditor/Core/Tilemap/ITileView.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "Core/Graphics/RenderImage.hpp"
+#include "OREMath/Rect.hpp"
+
+class ITileView {
+public:
+  ITileView() = default;
+
+  static RenderImage createRenderTexture(int width, int height);
+  static void clearRect(RenderImage& painter, const RectF& rect);
+
+  bool isTileDirty(int x, int y) const { return m_dirtyRect.contains({x, y}); }
+  void clearDirtyRect() { m_dirtyRect = Rect(); }
+  void setDirtyRect(const int x, const int y, const int width, const int height) { m_dirtyRect |= Rect(x, y, width, height); }
+
+private:
+  bool m_tileSizeChanged{false};
+  int m_blockWidth{96};
+  int m_blockHeight{96};
+  int m_tileWidth{48};
+  int m_tileHeight{48};
+  float m_tileScale{1.f};
+  RectF m_viewport;
+  Rect m_dirtyRect;
+};

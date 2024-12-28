962721db9ac90c5044b3c328bfcdb82b223a461c
Phillip Stephens <antidote.crk@gmail.com>
2024-12-28 11:57:36 -0800

    Fix Color build on windows

--- src/openrpgeditor/Platform/Windows/Resources.cpp
+++ src/openrpgeditor/Platform/Windows/Resources.cpp
@@ -10,14 +10,15 @@
 
 namespace App {
 
-static const std::string BASE_PATH{SDL_GetBasePath()};
-
 std::filesystem::path Resources::resource_path(const std::filesystem::path& file_path) {
-  std::filesystem::path font_path{BASE_PATH};
+  const char* base_path = SDL_GetBasePath();
+  std::filesystem::path font_path{base_path};
+  SDL_free((void*)base_path);
   font_path /= "../share" / file_path;
   return font_path;
 }
 
 std::filesystem::path Resources::font_path(const std::string_view& font_file) { return resource_path("fonts") / font_file; }
+std::filesystem::path Resources::image_path(const std::string_view& image_file) { return resource_path("images") / image_file; }
 
 } // namespace App

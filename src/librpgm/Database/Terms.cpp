bfa530b8f47935bdaf9876632c9611e1adad4918
Phillip Stephens <antidote.crk@gmail.com>
2025-01-12 15:20:00 -0800

    Terms IModifiable refactor

--- src/librpgm/Database/Terms.cpp
+++ src/librpgm/Database/Terms.cpp
@@ -1,17 +1,174 @@
 #include "Database/Terms.hpp"
 
+Terms::Terms(const Terms& other)
+: IModifiable(other)
+, m_basic(other.m_basic)
+, m_commands(other.m_commands)
+, m_messages(other.m_messages)
+, m_params(other.m_params)
+, m_oldbasic(other.m_oldbasic)
+, m_oldcommands(other.m_oldcommands)
+, m_oldmessages(other.m_oldmessages)
+, m_oldparams(other.m_oldparams) {}
+
+Terms& Terms::operator=(const Terms& other) {
+  IModifiable::operator=(other);
+  m_basic = other.m_basic;
+  m_commands = other.m_commands;
+  m_messages = other.m_messages;
+  m_params = other.m_params;
+  m_oldbasic = other.m_oldbasic;
+  m_oldcommands = other.m_oldcommands;
+  m_oldmessages = other.m_oldmessages;
+  m_oldparams = other.m_oldparams;
+  return *this;
+}
+
+Terms::Terms(Terms&& other) noexcept
+: IModifiable(std::move(other))
+, m_basic(std::move(other.m_basic))
+, m_commands(std::move(other.m_commands))
+, m_messages(std::move(other.m_messages))
+, m_params(std::move(other.m_params))
+, m_oldbasic(std::move(other.m_oldbasic))
+, m_oldcommands(std::move(other.m_oldcommands))
+, m_oldmessages(std::move(other.m_oldmessages))
+, m_oldparams(std::move(other.m_oldparams)) {}
+
+Terms& Terms::operator=(Terms&& other) noexcept {
+  IModifiable::operator=(std::move(other));
+  m_basic = std::move(other.m_basic);
+  m_commands = std::move(other.m_commands);
+  m_messages = std::move(other.m_messages);
+  m_params = std::move(other.m_params);
+  m_oldbasic = std::move(other.m_oldbasic);
+  m_oldcommands = std::move(other.m_oldcommands);
+  m_oldmessages = std::move(other.m_oldmessages);
+  m_oldparams = std::move(other.m_oldparams);
+  return *this;
+}
+
+std::vector<std::optional<std::string>>& Terms::basic() { return m_basic; }
+const std::vector<std::optional<std::string>>& Terms::basic() const { return m_basic; }
+void Terms::setBasic(std::vector<std::optional<std::string>>& value) {
+  if (m_basic == value) {
+    return;
+  }
+  MODIFIABLE_SET_OLD_VALUE(basic);
+  m_basic = value;
+  if (!signalsDisabled()) {
+    basicModified().fire(this, value);
+  }
+  setHasChanges();
+}
+
+std::vector<std::optional<std::string>>& Terms::commands() { return m_commands; }
+const std::vector<std::optional<std::string>>& Terms::commands() const { return m_commands; }
+void Terms::setCommands(std::vector<std::optional<std::string>>& value) {
+  if (m_commands == value) {
+    return;
+  }
+  MODIFIABLE_SET_OLD_VALUE(commands);
+  m_commands = value;
+  if (!signalsDisabled()) {
+    commandsModified().fire(this, value);
+  }
+  setHasChanges();
+}
+
+Messages& Terms::messages() { return m_messages; }
+const Messages& Terms::messages() const { return m_messages; }
+void Terms::setMessages(const Messages& value) {
+  if (m_messages == value) {
+    return;
+  }
+  MODIFIABLE_SET_OLD_VALUE(messages);
+  m_messages = value;
+  if (!signalsDisabled()) {
+    messagesModified().fire(this, value);
+  }
+  setHasChanges();
+}
+
+std::vector<std::optional<std::string>>& Terms::params() { return m_params; }
+const std::vector<std::optional<std::string>>& Terms::params() const { return m_params; }
+void Terms::setParams(std::vector<std::optional<std::string>>& value) {
+  if (m_params == value) {
+    return;
+  }
+  MODIFIABLE_SET_OLD_VALUE(params);
+  m_params = value;
+  if (!signalsDisabled()) {
+    paramsModified().fire(this, value);
+  }
+  setHasChanges();
+}
+
+void Terms::restoreOriginal() {
+  IModifiable::restoreOriginal();
+  MODIFIABLE_RESTORE_ORIGINAL_VALUE(basic);
+  MODIFIABLE_RESTORE_ORIGINAL_VALUE(commands);
+  MODIFIABLE_RESTORE_ORIGINAL_VALUE(messages);
+  MODIFIABLE_RESTORE_ORIGINAL_VALUE(params);
+  m_messages.restoreOriginal();
+}
+void Terms::acceptChanges() {
+  IModifiable::acceptChanges();
+  MODIFIABLE_ACCEPT_VALUE(basic);
+  MODIFIABLE_ACCEPT_VALUE(commands);
+  MODIFIABLE_ACCEPT_VALUE(messages);
+  MODIFIABLE_ACCEPT_VALUE(params);
+  m_messages.acceptChanges();
+}
+nlohmann::ordered_json Terms::serializeOldValues() const {
+  return {
+      MODIFIABLE_SERIALIZE_VALUE(basic),
+      MODIFIABLE_SERIALIZE_VALUE(commands),
+      MODIFIABLE_SERIALIZE_CHILD_VALUE(messages),
+      MODIFIABLE_SERIALIZE_VALUE(params),
+  };
+}
+
+rpgmutils::signal<void(Terms*, const std::vector<std::optional<std::string>>&)>& Terms::basicModified() {
+  if (!m_basicModified) {
+    m_basicModified.emplace();
+  }
+  return *m_basicModified;
+}
+
+rpgmutils::signal<void(Terms*, const std::vector<std::optional<std::string>>&)>& Terms::commandsModified() {
+  if (!m_commandsModified) {
+    m_commandsModified.emplace();
+  }
+  return *m_commandsModified;
+}
+
+rpgmutils::signal<void(Terms*, const Messages&)>& Terms::messagesModified() {
+  if (!m_messagesModified) {
+    m_messagesModified.emplace();
+  }
+  return *m_messagesModified;
+}
+
+rpgmutils::signal<void(Terms*, const std::vector<std::optional<std::string>>&)>& Terms::paramsModified() {
+  if (!m_paramsModified) {
+    m_paramsModified.emplace();
+  }
+  return *m_paramsModified;
+}
+
 void to_json(nlohmann::ordered_json& j, const Terms& terms) {
   j = {
-      {"basic", terms.basic},
-      {"commands", terms.commands},
-      {"messages", terms.messages},
-      {"params", terms.params},
+      {"basic", terms.m_basic},
+      {"commands", terms.m_commands},
+      {"messages", terms.m_messages},
+      {"params", terms.m_params},
   };
 }
 
 void from_json(const nlohmann::ordered_json& j, Terms& terms) {
-  terms.basic = j.value("basic", terms.basic);
-  terms.commands = j.value("commands", terms.commands);
-  terms.messages = j.value("messages", terms.messages);
-  terms.params = j.value("params", terms.params);
+  terms.m_basic = j.value("basic", terms.m_basic);
+  terms.m_commands = j.value("commands", terms.m_commands);
+  terms.m_messages = j.value("messages", terms.m_messages);
+  terms.m_params = j.value("params", terms.m_params);
 }
--- src/librpgm/Database/Terms.hpp
+++ src/librpgm/Database/Terms.hpp
@@ -11,12 +11,56 @@ class Terms final : public IModifiable {
   friend void from_json(const nlohmann::ordered_json& j, Terms& terms);
 
 public:
-  std::vector<std::optional<std::string>> basic;
-  std::vector<std::optional<std::string>> commands;
-  Messages messages;
-  std::vector<std::optional<std::string>> params;
+  Terms() = default;
+  Terms(const Terms& other);
+  Terms& operator=(const Terms& other);
+  Terms(Terms&& other) noexcept;
+  Terms& operator=(Terms&& other) noexcept;
 
-  bool operator==(const Terms& rhs) const { return basic == rhs.basic && commands == rhs.commands && params == rhs.params && messages == rhs.messages; }
+  std::vector<std::optional<std::string>>& basic();
+  const std::vector<std::optional<std::string>>& basic() const;
+  void setBasic(std::vector<std::optional<std::string>>& value);
+
+  std::vector<std::optional<std::string>>& commands();
+  const std::vector<std::optional<std::string>>& commands() const;
+  void setCommands(std::vector<std::optional<std::string>>& value);
+
+  Messages& messages();
+  const Messages& messages() const;
+  void setMessages(const Messages& value);
+
+  std::vector<std::optional<std::string>>& params();
+  const std::vector<std::optional<std::string>>& params() const;
+  void setParams(std::vector<std::optional<std::string>>& value);
+
+  void restoreOriginal() override;
+  void acceptChanges() override;
+  nlohmann::ordered_json serializeOldValues() const override;
+
+  bool isModified() const override { return IModifiable::isModified() | m_messages.isModified(); }
+
+  rpgmutils::signal<void(Terms*, const std::vector<std::optional<std::string>>&)>& basicModified();
+  rpgmutils::signal<void(Terms*, const std::vector<std::optional<std::string>>&)>& commandsModified();
+  rpgmutils::signal<void(Terms*, const Messages&)>& messagesModified();
+  rpgmutils::signal<void(Terms*, const std::vector<std::optional<std::string>>&)>& paramsModified();
+
+  bool operator==(const Terms& rhs) const { return m_basic == rhs.m_basic && m_commands == rhs.m_commands && m_params == rhs.m_params && m_messages == rhs.m_messages; }
+
+private:
+  std::vector<std::optional<std::string>> m_basic;
+  std::vector<std::optional<std::string>> m_commands;
+  Messages m_messages;
+  std::vector<std::optional<std::string>> m_params;
+
+  std::optional<std::vector<std::optional<std::string>>> m_oldbasic;
+  std::optional<std::vector<std::optional<std::string>>> m_oldcommands;
+  std::optional<Messages> m_oldmessages;
+  std::optional<std::vector<std::optional<std::string>>> m_oldparams;
+
+  std::optional<rpgmutils::signal<void(Terms*, const std::vector<std::optional<std::string>>&)>> m_basicModified;
+  std::optional<rpgmutils::signal<void(Terms*, const std::vector<std::optional<std::string>>&)>> m_commandsModified;
+  std::optional<rpgmutils::signal<void(Terms*, const Messages&)>> m_messagesModified;
+  std::optional<rpgmutils::signal<void(Terms*, const std::vector<std::optional<std::string>>&)>> m_paramsModified;
 };
 
 void to_json(nlohmann::ordered_json& j, const Terms& terms);

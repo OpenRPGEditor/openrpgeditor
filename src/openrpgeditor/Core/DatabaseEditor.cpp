39e40077cd67a024a62ff6cf1c6a3f527ed6445f
Phillip Stephens <antidote.crk@gmail.com>
2025-02-26 23:11:58 -0800

    Fix layout issues in switch/variable assignment tab

--- src/openrpgeditor/Core/DatabaseEditor.cpp
+++ src/openrpgeditor/Core/DatabaseEditor.cpp
@@ -27,61 +27,61 @@ void DatabaseEditor::draw() {
     const auto calc = ImGui::CalcTextSize("ABCDEFGHIJKLMNOPQR").x;
     ImGui::BeginChild("##orpg_database_editor_tab_buttons", ImVec2{calc + ImGui::GetStyle().ItemSpacing.x, 0}, 0, ImGuiWindowFlags_NoBackground);
     ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 100);
-    if (ImGui::SelectableWithBorder("Actors", m_currentTab == &m_actors.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Actors"), m_currentTab == &m_actors.value())) {
       m_currentTab = &m_actors.value();
     }
-    if (ImGui::SelectableWithBorder("Classes", m_currentTab == &m_classes.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Classes"), m_currentTab == &m_classes.value())) {
       m_currentTab = &m_classes.value();
     }
-    if (ImGui::SelectableWithBorder("Skills", m_currentTab == &m_skills.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Skills"), m_currentTab == &m_skills.value())) {
       m_currentTab = &m_skills.value();
     }
-    if (ImGui::SelectableWithBorder("Items", m_currentTab == &m_items.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Items"), m_currentTab == &m_items.value())) {
       m_currentTab = &m_items.value();
     }
-    if (ImGui::SelectableWithBorder("Weapons", m_currentTab == &m_weapons.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Weapons"), m_currentTab == &m_weapons.value())) {
       m_currentTab = &m_weapons.value();
     }
-    if (ImGui::SelectableWithBorder("Armors", m_currentTab == &m_armors.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Armors"), m_currentTab == &m_armors.value())) {
       m_currentTab = &m_armors.value();
     }
-    if (ImGui::SelectableWithBorder("Enemies", m_currentTab == &m_enemies.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Enemies"), m_currentTab == &m_enemies.value())) {
       m_currentTab = &m_enemies.value();
     }
-    if (ImGui::SelectableWithBorder("Troops", m_currentTab == &m_troops.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Troops"), m_currentTab == &m_troops.value())) {
       m_currentTab = &m_troops.value();
     }
-    if (ImGui::SelectableWithBorder("States", m_currentTab == &m_states.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("States"), m_currentTab == &m_states.value())) {
       m_currentTab = &m_states.value();
     }
-    if (ImGui::SelectableWithBorder("Animations", m_currentTab == &m_animations.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Animations"), m_currentTab == &m_animations.value())) {
       m_currentTab = &m_animations.value();
     }
-    if (ImGui::SelectableWithBorder("Tilesets", m_currentTab == &m_tilesets.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Tilesets"), m_currentTab == &m_tilesets.value())) {
       m_currentTab = &m_tilesets.value();
     }
-    if (ImGui::SelectableWithBorder("Common Events", m_currentTab == &m_commonEvents.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Common Events"), m_currentTab == &m_commonEvents.value())) {
       m_currentTab = &m_commonEvents.value();
     }
-    if (ImGui::SelectableWithBorder("System", m_currentTab == &m_system.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("System"), m_currentTab == &m_system.value())) {
       m_currentTab = &m_system.value();
     }
-    if (ImGui::SelectableWithBorder("Types", m_currentTab == &m_types.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Types"), m_currentTab == &m_types.value())) {
       m_currentTab = &m_types.value();
     }
-    if (ImGui::SelectableWithBorder("Terms", m_currentTab == &m_terms.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Terms"), m_currentTab == &m_terms.value())) {
       m_currentTab = &m_terms.value();
     }
-    if (ImGui::SelectableWithBorder("Exported Constants", m_currentTab == &m_gameConstants.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Exported Constants"), m_currentTab == &m_gameConstants.value())) {
       m_currentTab = &m_gameConstants.value();
     }
-    if (ImGui::SelectableWithBorder("Templates", m_currentTab == &m_templates.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Templates"), m_currentTab == &m_templates.value())) {
       m_currentTab = &m_templates.value();
     }
-    if (ImGui::SelectableWithBorder("Mapping", m_currentTab == &m_mappings.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Switches & Variables"), m_currentTab == &m_mappings.value())) {
       m_currentTab = &m_mappings.value();
     }
-    if (ImGui::SelectableWithBorder("Documentation", m_currentTab == &m_docs.value())) {
+    if (ImGui::SelectableWithBorder(trNOOP("Documentation"), m_currentTab == &m_docs.value())) {
       m_currentTab = &m_docs.value();
     }
     ImGui::EndChild();
--- src/openrpgeditor/Core/DatabaseEditor/DBMappingsTab.cpp
+++ src/openrpgeditor/Core/DatabaseEditor/DBMappingsTab.cpp
@@ -17,77 +17,86 @@ void DBMappingsTab::draw() {
     if (ImGui::BeginTabBar("##orpg_mappings_editor_tabs")) {
 
       if (ImGui::BeginTabItem("Assignment")) {
-
-        ImGui::BeginChild("##orpg_mapping_header_panel_left", ImVec2{ImGui::GetContentRegionMax().x / 2, 60});
+        ImGui::BeginChild("##orpg_mapping_header",
+                          ImVec2{ImGui::GetContentRegionAvail().x, ImGui::GetContentRegionAvail().y - (calc.y * 3 + ImGui::GetStyle().ItemSpacing.y * 3 + ImGui::GetStyle().FramePadding.y)});
         {
-          ImGui::SeparatorText("Variables");
-          ImGui::PushItemWidth(ImGui::GetContentRegionMax().x - 20);
-          if (ImGui::InputText("##orpg_mapping_variable_edit_string", &m_variable_string)) {
-            m_system.setVariable(m_selectedVariable, m_variable_string);
-          }
-          ImGui::PopItemWidth();
-        }
-        ImGui::EndChild();
-        ImGui::SameLine();
-        ImGui::BeginChild("##orpg_mapping_header_panel_right", ImVec2{ImGui::GetContentRegionMax().x / 2, 60});
-        {
-          ImGui::SeparatorText("Switches");
-          ImGui::PushItemWidth(ImGui::GetContentRegionMax().x - 20);
-          if (ImGui::InputText("##orpg_mapping_switch_edit_string", &m_switch_string)) {
-            m_system.setSwitch(m_selectedSwitch, m_switch_string);
-          }
-          ImGui::PopItemWidth();
-        }
-        ImGui::EndChild();
-        ImGui::BeginChild("##orpg_mapping_variable_panel_left", ImVec2{ImGui::GetContentRegionMax().x / 2, ImGui::GetContentRegionMax().y - 170.f});
-        {
-
-          int index{0};
-          for (auto& variable : m_system.variables()) {
-            if (index > 0) {
-              if (ImGui::Selectable(std::format("{}##orpg_mappings_variable_{}", variable, index).c_str(), m_selectedVariable == index || (ImGui::IsItemFocused() && m_selectedVariable != index))) {
-                m_selectedVariable = index;
-                m_variable_string = variable;
-              }
+          ImGui::BeginChild("##orpg_mapping_header_panel_left", ImVec2{ImGui::GetContentRegionAvail().x / 2, calc.y * 3 + ImGui::GetStyle().ItemSpacing.y}, ImGuiChildFlags_AlwaysAutoResize);
+          {
+            ImGui::SeparatorText("Variables");
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x - ImGui::GetStyle().FramePadding.x);
+            if (ImGui::InputText("##orpg_mapping_variable_edit_string", &m_variable_string)) {
+              m_system.setVariable(m_selectedVariable, m_variable_string);
             }
-            index++;
+            ImGui::PopItemWidth();
           }
-        }
-        ImGui::EndChild();
-        ImGui::SameLine();
-        ImGui::BeginChild("##orpg_mapping_switch_panel_right", ImVec2{ImGui::GetContentRegionMax().x / 2 - 10, ImGui::GetContentRegionMax().y - 170.f});
-        {
-
-          int index{0};
-          for (auto& sw : m_system.switches()) {
-            if (index > 0) {
-              if (ImGui::Selectable(std::format("{}##orpg_mappings_switch_{}", sw, index).c_str(), m_selectedSwitch == index || (ImGui::IsItemFocused() && m_selectedSwitch != index))) {
-                m_selectedSwitch = index;
-                m_switch_string = sw;
-              }
+          ImGui::EndChild();
+          ImGui::SameLine();
+          ImGui::BeginChild("##orpg_mapping_header_panel_right", ImVec2{ImGui::GetContentRegionMax().x / 2, calc.y * 3 + ImGui::GetStyle().ItemSpacing.y});
+          {
+            ImGui::SeparatorText("Switches");
+            ImGui::PushItemWidth(ImGui::GetContentRegionMax().x - ImGui::GetStyle().FramePadding.x);
+            if (ImGui::InputText("##orpg_mapping_switch_edit_string", &m_switch_string)) {
+              m_system.setSwitch(m_selectedSwitch, m_switch_string);
             }
-            index++;
+            ImGui::PopItemWidth();
           }
-        }
-        ImGui::EndChild();
-        ImGui::BeginChild("##orpg_mapping_footer_panel_left", ImVec2{ImGui::GetContentRegionMax().x / 2, 60});
-        {
-          ImGui::SeparatorText(trFormat("Max Switches {}", m_system.switches().size() - 1).c_str());
-          if (ImGui::Button("Change Max", ImVec2{ImGui::GetContentRegionMax().x - 8, 0})) {
-            m_changeIntDialogOpen = true;
-            m_editMaxSwitches = m_system.switches().size() - 1;
-          }
-        }
-        ImGui::EndChild();
-        ImGui::SameLine();
-        ImGui::BeginChild("##orpg_mapping_footer_panel_right", ImVec2{ImGui::GetContentRegionMax().x / 2 - 10, 60});
-        {
+          ImGui::EndChild();
+          ImGui::BeginChild("##orpg_mapping_variable_panel_left", ImVec2{ImGui::GetContentRegionMax().x / 2, ImGui::GetContentRegionAvail().y - ImGui::GetStyle().FramePadding.y});
+          {
 
-          ImGui::SeparatorText(trFormat("Max Variables {}", m_system.switches().size() - 1).c_str());
-          if (ImGui::Button("Change Max", ImVec2{ImGui::GetContentRegionMax().x - 8, 0})) {
-            m_changeIntDialogOpen = true;
-            m_editMaxVariables = m_system.variables().size() - 1;
+            int index{0};
+            for (auto& variable : m_system.variables()) {
+              if (index > 0) {
+                if (ImGui::Selectable(std::format("{}##orpg_mappings_variable_{}", variable, index).c_str(), m_selectedVariable == index || (ImGui::IsItemFocused() && m_selectedVariable != index))) {
+                  m_selectedVariable = index;
+                  m_variable_string = variable;
+                }
+              }
+              index++;
+            }
           }
+          ImGui::EndChild();
+          ImGui::SameLine();
+          ImGui::BeginChild("##orpg_mapping_switch_panel_right",
+                            ImVec2{ImGui::GetContentRegionMax().x / 2 - ImGui::GetStyle().FramePadding.x, ImGui::GetContentRegionAvail().y - ImGui::GetStyle().FramePadding.y});
+          {
+
+            int index{0};
+            for (auto& sw : m_system.switches()) {
+              if (index > 0) {
+                if (ImGui::Selectable(std::format("{}##orpg_mappings_switch_{}", sw, index).c_str(), m_selectedSwitch == index || (ImGui::IsItemFocused() && m_selectedSwitch != index))) {
+                  m_selectedSwitch = index;
+                  m_switch_string = sw;
+                }
+              }
+              index++;
+            }
+          }
+          ImGui::EndChild();
+        }
+        ImGui::EndChild();
+        ImGui::BeginChild("##orpg_mappings_footer");
+        {
+          ImGui::BeginChild("##orpg_mapping_footer_panel_left", ImVec2{ImGui::GetContentRegionMax().x / 2, 0});
+          {
+            ImGui::SeparatorText(trFormat("Max Switches {}", m_system.switches().size() - 1).c_str());
+            if (ImGui::Button("Change Max", ImVec2{ImGui::GetContentRegionMax().x - 8, 0})) {
+              m_changeIntDialogOpen = true;
+              m_editMaxSwitches = m_system.switches().size() - 1;
+            }
+          }
+          ImGui::EndChild();
+          ImGui::SameLine();
+          ImGui::BeginChild("##orpg_mapping_footer_panel_right", ImVec2{ImGui::GetContentRegionMax().x / 2 - 10, 0});
+          {
+
+            ImGui::SeparatorText(trFormat("Max Variables {}", m_system.switches().size() - 1).c_str());
+            if (ImGui::Button("Change Max", ImVec2{ImGui::GetContentRegionMax().x - 8, 0})) {
+              m_changeIntDialogOpen = true;
+              m_editMaxVariables = m_system.variables().size() - 1;
+            }
+          }
+          ImGui::EndChild();
         }
         ImGui::EndChild();
 

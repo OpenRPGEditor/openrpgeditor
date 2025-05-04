ff179fac0b3df03cf0396473b2ac6a7232338b96
Phillip Stephens <antidote.crk@gmail.com>
2025-05-04 09:41:24 -0700

    Fix StopBGM and ResumeBGM

--- src/openrpgeditor/Core/EventCommands/Dialog_ReplayBGM.cpp
+++ src/openrpgeditor/Core/EventCommands/Dialog_ReplayBGM.cpp
@@ -1,4 +1,10 @@
 #include "Core/EventCommands/Dialog_ReplayBGM.hpp"
 #include <tuple>
 
-std::tuple<bool, bool> Dialog_ReplayBGM::draw() { return std::make_tuple(!m_open, m_confirmed); }
+std::tuple<bool, bool> Dialog_ReplayBGM::draw() {
+  if (m_open) {
+    m_open = false;
+    m_confirmed = true;
+  }
+  return std::make_tuple(!m_open, m_confirmed);
+}
--- src/openrpgeditor/Core/EventCommands/Dialog_StopSE.cpp
+++ src/openrpgeditor/Core/EventCommands/Dialog_StopSE.cpp
@@ -1,4 +1,10 @@
 #include "Core/EventCommands/Dialog_StopSE.hpp"
 #include <tuple>
 
-std::tuple<bool, bool> Dialog_StopSE::draw() { return std::make_tuple(!m_open, m_confirmed); }
+std::tuple<bool, bool> Dialog_StopSE::draw() {
+  if (m_open) {
+    m_open = false;
+    m_confirmed = true;
+  }
+  return std::make_tuple(!m_open, m_confirmed);
+}

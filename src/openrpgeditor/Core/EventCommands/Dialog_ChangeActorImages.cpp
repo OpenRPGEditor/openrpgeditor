58c29e2e17ecfaadcdad213f80597db8f6905710
Phillip Stephens <antidote.crk@gmail.com>
2025-08-10 08:39:54 -0700

    Rework ChangeActorImages dialog

--- src/openrpgeditor/Core/EventCommands/Dialog_ChangeActorImages.cpp
+++ src/openrpgeditor/Core/EventCommands/Dialog_ChangeActorImages.cpp
@@ -1,141 +1,184 @@
 #include "Core/EventCommands/Dialog_ChangeActorImages.hpp"
 #include "Core/Application.hpp"
 
-#include "Core/Log.hpp"
-#include "imgui.h"
+#include "Core/CommonUI/GroupBox.hpp"
+#include "Core/ImGuiExt/ImGuiUtils.hpp"
+#include "Database/Database.hpp"
+#include <imgui.h>
+#include <imgui_internal.h>
+
 #include <tuple>
 
 std::tuple<bool, bool> Dialog_ChangeActorImages::draw() {
   if (isOpen()) {
-    ImGui::OpenPopup(m_name.c_str());
+    ImGui::OpenPopup("###ChangeActorImages");
   }
-  ImVec2 center = ImGui::GetMainViewport()->GetCenter();
-  ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
-  ImGui::SetNextWindowSize(ImVec2{400, 225}, ImGuiCond_Appearing);
-  if (ImGui::BeginPopupModal(m_name.c_str(), &m_open, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize)) {
+  const auto imageSize = ImGui::GetDPIScaledSize(FaceSheet::faceWidth(), FaceSheet::faceHeight());
+  ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+  const auto maxSize = ImVec2{(imageSize.x * 4) + (ImGui::GetStyle().FramePadding.x * 2), imageSize.y + (ImGui::GetFrameHeightWithSpacing() * 8) + (ImGui::GetStyle().FramePadding.y * 2)};
+  ImGui::SetNextWindowSize(maxSize, ImGuiCond_Appearing);
+  ImGui::SetNextWindowSizeConstraints(maxSize, {FLT_MAX, FLT_MAX});
+  m_faceButton.setSize(imageSize.x, imageSize.y);
+  m_characterButton.setSize(imageSize.x, imageSize.y);
+  m_battlerButton.setSize(imageSize.x, imageSize.y);
 
-    if (const auto [closed, confirmed] = m_characterPicker.draw(); closed) {
-      if (confirmed) {
-        m_characterPicker.accept();
-        if (m_image_selection == 0) {
-          m_faceIndex = m_characterPicker.character();
-          m_facePicture = m_characterPicker.selectedSheet();
-          m_faceSheet = FaceSheet(m_facePicture);
-        } else if (m_image_selection == 1) {
-          m_charIndex = m_characterPicker.character();
-          m_charPicture = m_characterPicker.selectedSheet();
-          m_characterSheet = CharacterSheet(m_charPicture);
-        } else {
-          m_battlerPicture = m_characterPicker.selectedSheet();
-          m_battlerSheet = SideViewBattlerSheet(m_battlerPicture);
-        }
-      }
+  if (ImGui::BeginPopupModal(std::format("{}###ChangeActorImages", m_name).c_str(), &m_open, ImGuiWindowFlags_NoResize)) {
+    drawPickers();
+    if (!m_faceSheet && !m_facePicture.empty() && !m_faceButton.hasCompositeTextures()) {
+      m_faceSheet.emplace(m_facePicture);
+      auto offset = m_faceSheet->getFaceRect(m_faceIndex).min;
+      offset.xr() *= m_faceSheet->texture().width();
+      offset.yr() *= m_faceSheet->texture().height();
+      m_faceButton.setTexturesToComposite({
+          {
+              m_faceSheet->texture(),
+              Point{m_faceSheet->faceWidth(), m_faceSheet->faceHeight()},
+              offset.toPoint(),
+              true,
+          },
+      });
     }
-    if (actor_picker) {
-      auto [closed, confirmed] = actor_picker->draw();
-      if (closed) {
-        if (confirmed) {
-          m_actor = actor_picker->selection();
-        }
-        actor_picker.reset();
-      }
-    }
-    ImGui::PushID("##actor_selection");
-    if (ImGui::Button((std::format("{:04} ", m_actor) + Database::instance()->actorName(m_actor)).c_str(), {(160), 0})) {
 
-      actor_picker = ObjectPicker<Actor>("Actor"sv, Database::instance()->actors.actorList(), m_actor);
-      actor_picker->setOpen(true);
+    if (!m_characterSheet && !m_charPicture.empty() && !m_characterButton.hasCompositeTextures()) {
+      m_characterSheet.emplace(m_charPicture);
+      const auto [min, max] = m_characterSheet->getRectForCharacter(m_charIndex, 1);
+      m_characterButton.setTexturesToComposite({{m_characterSheet->texture(),
+                                                 {m_characterSheet->characterWidth(), m_characterSheet->characterHeight()},
+                                                 {static_cast<int>(min.x() * m_characterSheet->texture().width()), static_cast<int>(min.y() * m_characterSheet->texture().height())}}});
     }
-    ImGui::PopID();
-    ImGui::BeginGroup();
+    if (!m_battlerSheet && !m_battlerPicture.empty() && !m_battlerButton.hasCompositeTextures()) {
+      m_battlerSheet.emplace(m_battlerPicture);
+      const auto [_, frames] = m_battlerSheet->getAction(SideViewActionType::StepForward);
+      m_battlerButton.setTexturesToComposite({{m_battlerSheet->texture(),
+                                               {m_battlerSheet->characterWidth(), m_battlerSheet->characterHeight()},
+                                               {static_cast<int>(frames[1].min.x() * m_battlerSheet->texture().width()), static_cast<int>(frames[1].min.y() * m_battlerSheet->texture().height())}}});
+    }
+
+    ImGui::BeginVertical("##change_actor_images_main_layout", ImGui::GetContentRegionAvail(), 0);
     {
-      ImGui::SeparatorText("Images");
-      const auto buttonSize = ImVec2{144, 144};
-      const auto buttonCenter = (buttonSize / 2);
-      ImGui::BeginGroup();
-      {
-
-        ImGui::Text("Face:");
-        auto cursorPos = ImGui::GetCursorPos();
-        if (ImGui::ImageButton("##face_image", static_cast<ImTextureID>(m_buttonBack), ImVec2{80.f, 102.f})) {
-          m_image_selection = 0;
-          m_characterPicker.setCharacterInfo(m_charPicture, m_actor);
-          m_characterPicker.setOpen(true);
-        }
-        if (m_faceSheet && m_faceSheet->texture()) {
-          const auto faceRect = ImVec2{static_cast<float>(m_faceSheet->faceWidth()), static_cast<float>(m_faceSheet->faceHeight())};
-          ImGui::SetCursorPos(((cursorPos + buttonCenter) - (faceRect / 2)) + (ImGui::GetStyle().ItemInnerSpacing - ImVec2{0.f, 1.f}));
-          const auto rect = m_faceSheet->getFaceRect(m_faceIndex);
-          ImGui::Image(m_faceSheet->texture(), faceRect, rect.min, rect.max);
+      GroupBox actorGroupBox(trNOOP("Actor"), "##change_actor_images_actor_group", {-1, 0});
+      if (actorGroupBox.begin()) {
+        if (ImGui::EllipsesButton(std::format("{}##change_actor_iamges_actor", Database::instance()->actorNameAndId(m_actor)).c_str(), {-1, 0})) {
+          m_actorPicker = ObjectPicker(trNOOP("Actors"), Database::instance()->actors.actorList(), m_actor);
+          m_actorPicker->setOpen(true);
         }
       }
-      ImGui::EndGroup();
-      ImGui::SameLine();
-      ImGui::BeginGroup();
-      {
-        ImGui::Text("Character:");
-        auto cursorPos = ImGui::GetCursorPos();
-        if (ImGui::ImageButton("##char_image", static_cast<ImTextureID>(m_buttonBack), ImVec2{80.f, 102.f})) {
-          m_image_selection = 1;
-          m_characterPicker.setCharacterInfo(m_facePicture, m_charIndex);
-          m_characterPicker.setOpen(true);
-        }
-        if (m_characterSheet && m_characterSheet->texture()) {
-          if (m_characterSheet->characterWidth() < 72 || m_characterSheet->characterHeight() < 96) {
-            ImGui::SetCursorPos(cursorPos + (ImVec2{m_characterSheet->characterWidth() / 2.f, m_characterSheet->characterHeight() / 2.f}));
-          } else {
-            ImGui::SetCursorPos(cursorPos);
+      actorGroupBox.end();
+      GroupBox imagesGroup(trNOOP("Images"), "##change_actor_images_images_group", {-1, 0});
+      if (imagesGroup.begin()) {
+        ImGui::BeginHorizontal("##change_actor_images_images_layout", {-1, 0}, 0);
+        {
+          GroupBox faceGroup(trNOOP("Face"), "##change_actor_images_face_group");
+          if (faceGroup.begin()) {
+            if (ImGui::ImageButtonEx(ImGui::GetID("##face_image"), m_faceButton.get(), imageSize, {0.f, 0.f}, {1.f, 1.f}, {0.f, 0.f, 0.f, 0.f}, {1.f, 1.f, 1.f, 1.f},
+                                     ImGuiButtonFlags_PressedOnDoubleClick)) {
+              m_facePicker.setFaceInfo(m_facePicture, m_faceIndex);
+              m_facePicker.setOpen(true);
+            }
           }
-
-          const auto [min, max] = m_characterSheet->getRectForCharacter(m_charIndex);
-
-          ImGui::Image(m_characterSheet->texture(), ImVec2{static_cast<float>(m_characterSheet->characterWidth()), static_cast<float>(m_characterSheet->characterHeight())}, min, max);
+          faceGroup.end();
+          GroupBox characterGroup(trNOOP("Character"), "##change_actor_images_character_group");
+          if (characterGroup.begin()) {
+            if (ImGui::ImageButtonEx(ImGui::GetID("##char_image"), m_characterButton.get(), imageSize, {0.f, 0.f}, {1.f, 1.f}, {0.f, 0.f, 0.f, 0.f}, {1.f, 1.f, 1.f, 1.f},
+                                     ImGuiButtonFlags_PressedOnDoubleClick)) {
+              m_characterPicker.setCharacterInfo(m_facePicture, m_charIndex);
+              m_characterPicker.setOpen(true);
+            }
+          }
+          characterGroup.end();
+          GroupBox battlerGroup(trNOOP("[SV] Battler"), "##change_actor_images_battler_group");
+          if (battlerGroup.begin()) {
+            if (ImGui::ImageButtonEx(ImGui::GetID("##battler_image"), m_battlerButton.get(), imageSize, {0.f, 0.f}, {1.f, 1.f}, {0.f, 0.f, 0.f, 0.f}, {1.f, 1.f, 1.f, 1.f},
+                                     ImGuiButtonFlags_PressedOnDoubleClick)) {
+              m_battlerPicker.setImageInfo(m_battlerPicture);
+              m_battlerPicker.setOpen(true);
+            }
+          }
+          battlerGroup.end();
         }
+        ImGui::EndHorizontal();
       }
-      ImGui::EndGroup();
-      ImGui::SameLine();
-      ImGui::BeginGroup();
+      imagesGroup.end();
+      ImGui::Spring();
+      ImGui::SeparatorEx(ImGuiSeparatorFlags_Horizontal, ImGui::GetDPIScaledValue(1.5f));
+      ImGui::BeginHorizontal("##change_actor_images_button_layout", {-1, 0}, 0);
       {
-        ImGui::Text("[SV] Battler:");
-        auto cursorPos = ImGui::GetCursorPos();
-        if (ImGui::ImageButton("##svbattler_image", static_cast<ImTextureID>(m_buttonBack), ImVec2{80.f, 102.f})) {
-          m_image_selection = 2;
-          m_characterPicker.setCharacterInfo(m_battlerPicture, m_faceIndex);
-          m_characterPicker.setOpen(true);
+        ImGui::Spring();
+        if (const auto ret = ImGui::ButtonGroup("##scroll_map_buttons", {trNOOP("OK"), trNOOP("Cancel")}); ret == 0) {
+          m_confirmed = true;
+          m_command->actor = m_actor;
+          m_command->charPicture = m_charPicture;
+          m_command->charIndex = m_charIndex;
+          m_command->facePicture = m_facePicture;
+          m_command->faceIndex = m_faceIndex;
+          m_command->battlerPicture = m_battlerPicture;
+          ImGui::CloseCurrentPopup();
+          setOpen(false);
+        } else if (ret == 1) {
+          ImGui::CloseCurrentPopup();
+          setOpen(false);
         }
-        if (m_battlerSheet && m_battlerSheet->texture()) {
-          const auto battlerRect = ImVec2{static_cast<float>(m_battlerSheet->characterWidth()) * 2, static_cast<float>(m_battlerSheet->characterHeight()) * 2};
-          ImGui::SetCursorPos((cursorPos + buttonCenter) - (battlerRect / 2));
-          const auto rect = m_battlerSheet->getAction(SideViewActionType::StepForward);
-          ImGui::Image(m_battlerSheet->texture(), battlerRect, rect.frames[1].min, rect.frames[1].max);
-        }
       }
-      ImGui::EndGroup();
+      ImGui::EndHorizontal();
     }
-    ImGui::EndGroup();
-    ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 2.f);
-    ImGui::BeginGroup();
-    {
-      if (ImGui::Button("OK")) {
-        m_confirmed = true;
-        command->actor = m_actor;
-        command->charPicture = m_charPicture;
-        command->charIndex = m_charIndex;
-        command->facePicture = m_facePicture;
-        command->faceIndex = m_faceIndex;
-        command->battlerPicture = m_battlerPicture;
-        ImGui::CloseCurrentPopup();
-        setOpen(false);
-      }
-      ImGui::SameLine();
-      if (ImGui::Button("Cancel")) {
-        ImGui::CloseCurrentPopup();
-        setOpen(false);
-      }
-    }
-    ImGui::EndGroup();
+    ImGui::EndVertical();
     ImGui::EndPopup();
   }
 
   return std::make_tuple(!m_open, m_confirmed);
+}
+
+void Dialog_ChangeActorImages::drawPickers() {
+  if (const auto [closed, confirmed] = m_facePicker.draw(); closed) {
+    if (confirmed) {
+      m_facePicker.accept();
+      m_faceIndex = m_characterPicker.character();
+      m_facePicture = m_characterPicker.selectedSheet();
+      m_faceSheet.reset();
+      m_faceButton.clear();
+    }
+  }
+  if (const auto [closed, confirmed] = m_characterPicker.draw(); closed) {
+    if (confirmed) {
+      m_characterPicker.accept();
+      if (m_imageSelection == 0) {
+        m_charIndex = m_characterPicker.character();
+        m_charPicture = m_characterPicker.selectedSheet();
+        m_characterSheet.reset();
+        m_characterButton.clear();
+      }
+    }
+  }
+
+  if (const auto [closed, confirmed] = m_battlerPicker.draw(); closed) {
+    if (confirmed) {
+      m_battlerPicker.accept();
+      m_battlerPicture = m_battlerPicker.selectedImage();
+      m_battlerSheet.reset();
+      m_battlerButton.clear();
+    }
+  }
+  if (m_actorPicker) {
+
+    if (const auto [closed, confirmed] = m_actorPicker->draw(); closed) {
+      if (confirmed) {
+        if (m_actor != m_actorPicker->selection()) {
+          m_actor = m_actorPicker->selection();
+          const auto actor = Database::instance()->actors.actor(m_actor);
+          m_charPicture = actor->characterName();
+          m_charIndex = actor->characterIndex();
+          m_facePicture = actor->faceName();
+          m_faceIndex = actor->faceIndex();
+          m_battlerPicture = actor->battlerName();
+          m_faceSheet.reset();
+          m_faceButton.clear();
+          m_characterSheet.reset();
+          m_characterButton.clear();
+          m_battlerSheet.reset();
+          m_battlerButton.clear();
+        }
+      }
+      m_actorPicker.reset();
+    }
+  }
 }
--- src/openrpgeditor/Core/EventCommands/Dialog_ChangeActorImages.hpp
+++ src/openrpgeditor/Core/EventCommands/Dialog_ChangeActorImages.hpp
@@ -1,34 +1,45 @@
 #pragma once
 #include "Core/CommonUI/CharacterPicker.hpp"
+#include "Core/CommonUI/FacePicker.hpp"
+#include "Core/CommonUI/ImagePicker.hpp"
 #include "Core/CommonUI/ObjectPicker.hpp"
 #include "Core/CommonUI/TextEditor.hpp"
 #include "Core/EventCommands/IEventDialogController.hpp"
 #include "Core/Graphics/CharacterSheet.hpp"
+#include "Core/Graphics/CheckeredCompositeTexture.hpp"
 #include "Core/Graphics/FaceSheet.hpp"
 #include "Core/Graphics/SideViewBattlerSheet.hpp"
 #include "Database/Actors.hpp"
+#include "Database/Database.hpp"
 #include "Database/EventCommands/ChangeActorImage.hpp"
 
 struct Dialog_ChangeActorImages : IEventDialogController {
   Dialog_ChangeActorImages() = delete;
   explicit Dialog_ChangeActorImages(const std::string& name, const std::shared_ptr<ChangeActorImageCommand>& cmd = nullptr)
   : IEventDialogController(name)
-  , command(cmd) {
+  , m_command(cmd) {
     if (cmd == nullptr) {
-      command.reset(new ChangeActorImageCommand());
+      m_command.reset(new ChangeActorImageCommand());
+      const auto actor = Database::instance()->actors.actor(m_command->actor);
+      m_command->charPicture = actor->characterName();
+      m_command->charIndex = actor->characterIndex();
+      m_command->facePicture = actor->faceName();
+      m_command->faceIndex = actor->faceIndex();
+      m_command->battlerPicture = actor->battlerName();
     }
-    m_actor = command->actor;
-    m_charPicture = command->charPicture;
-    m_charIndex = command->charIndex;
-    m_facePicture = command->facePicture;
-    m_faceIndex = command->faceIndex;
-    m_battlerPicture = command->battlerPicture;
+    m_actor = m_command->actor;
+    m_charPicture = m_command->charPicture;
+    m_charIndex = m_command->charIndex;
+    m_facePicture = m_command->facePicture;
+    m_faceIndex = m_command->faceIndex;
+    m_battlerPicture = m_command->battlerPicture;
   }
   std::tuple<bool, bool> draw() override;
 
-  std::shared_ptr<IEventCommand> getCommand() override { return command; };
+  std::shared_ptr<IEventCommand> getCommand() override { return m_command; };
 
 private:
+  void drawPickers();
   int m_actor;
   std::string m_charPicture;
   int m_charIndex;
@@ -36,17 +47,21 @@ private:
   int m_faceIndex;
   std::string m_battlerPicture;
 
-  int m_image_selection{0};
+  int m_imageSelection{0};
 
-  CheckerboardTexture m_buttonBack{80, 102, CellSizes::_64, 255, 200};
+  CheckeredCompositeTexture m_faceButton;
+  CheckeredCompositeTexture m_characterButton;
+  CheckeredCompositeTexture m_battlerButton;
+
+  FacePicker m_facePicker;
   CharacterPicker m_characterPicker{CharacterPicker::PickerMode::Character};
+  ImagePicker m_battlerPicker{ImagePicker::PickerMode::SVBattler};
 
   std::optional<CharacterSheet> m_characterSheet;
   std::optional<FaceSheet> m_faceSheet;
   std::optional<SideViewBattlerSheet> m_battlerSheet;
 
   bool m_confirmed{false};
-  std::optional<ObjectPicker<Actor>> actor_picker;
-  std::shared_ptr<ChangeActorImageCommand> command;
-  std::tuple<bool, bool> result;
+  std::optional<ObjectPicker<Actor>> m_actorPicker;
+  std::shared_ptr<ChangeActorImageCommand> m_command;
 };

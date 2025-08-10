337d5e7ff072dc8014596f45e62115ec31b96724
Phillip Stephens <antidote.crk@gmail.com>
2025-08-10 16:01:02 -0700

    Rework MovementChangeSpeed dialog

--- src/openrpgeditor/Core/EventCommands/MovementRoute/Dialog_MovementChangeSpeed.hpp
+++ src/openrpgeditor/Core/EventCommands/MovementRoute/Dialog_MovementChangeSpeed.hpp
@@ -1,25 +1,24 @@
 #pragma once
-#include "Core/CommonUI/TextEditor.hpp"
+
 #include "Core/EventCommands/IEventDialogController.hpp"
 #include "Database/EventCommands/MovementRoute/Speed.hpp"
-struct MainWindow;
-struct Dialog_MovementChangeSpeed : IEventDialogController {
+
+struct Dialog_MovementChangeSpeed final : IEventDialogController {
   Dialog_MovementChangeSpeed() = delete;
   explicit Dialog_MovementChangeSpeed(const std::string& name, const std::shared_ptr<MovementSpeedCommand>& cmd = nullptr)
   : IEventDialogController(name)
-  , command(cmd) {
-    if (command == nullptr) {
-      command.reset(new MovementSpeedCommand());
+  , m_command(cmd) {
+    if (m_command == nullptr) {
+      m_command.reset(new MovementSpeedCommand());
     }
-    m_speed = command->speed;
+    m_speed = m_command->speed;
   }
   std::tuple<bool, bool> draw() override;
 
-  std::shared_ptr<IEventCommand> getCommand() override { return command; };
+  std::shared_ptr<IEventCommand> getCommand() override { return m_command; };
 
 private:
   int m_speed;
   bool m_confirmed{false};
-  std::shared_ptr<MovementSpeedCommand> command;
-  std::tuple<bool, bool> result;
+  std::shared_ptr<MovementSpeedCommand> m_command;
 };
